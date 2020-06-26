915f0fb442429f92febdc3051af409f09d3d35f3
cfue <curtis.ch.chat@gmail.com>
2020-06-26 10:59:56 -0600

    product routes

---
+++ routes/admin/products.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/admin/products", (req, res) => {});
+
+router.get("/admin/products/new", (req, res) => {});
+
+// Routes to add
+// 1. list out products
+// 2. show a form to create new product
+// 3. submit form
+// 4. edit form
+// 5. submitting edited form
+// 6. deleting a product
