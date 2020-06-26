a4ea5ed70b035ed496a2a43069a023e8fdbf1ef4
cfue <curtis.ch.chat@gmail.com>
2020-06-26 12:42:55 -0600

    added repository.js

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -6,6 +6,8 @@ router.get("/admin/products", (req, res) => {});
 
 router.get("/admin/products/new", (req, res) => {});
 
+module.exports = router;
+
 // Routes to add
 // 1. list out products
 // 2. show a form to create new product
